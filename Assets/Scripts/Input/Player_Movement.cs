09c80dba9f3432da3611d43926eaf1c52f6b3c8d
Bredian <sasha.kry@gmail.com>
2020-07-29 00:32:32 +0700

    Player move effect + Movement rewhrite
    Created cool trial effect in particle system, slightly optimed Player_Movement, commented it for idiots like me.

--- Assets/Scripts/Input/Player_Movement.cs
+++ Assets/Scripts/Input/Player_Movement.cs
@@ -5,49 +5,64 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Radius around tapPosition where you need stop moving
+    [SerializeField] private float deltaRadius;
+    //New Input Systems Actions called InputMaster
     private InputMaster inputMaster;
-    
+    //Move flag
     private bool moving = false;
+    //Rotate hero flag
+    private bool needRotation = false;
+    //Position where you tapped
     private Vector3 tapPosition;
-    [SerializeField] float deltaRadius;
+    //Speed magnitude
     public float speed = 2;
-    void Start()
-    {
-        
-    }
     void Awake()
     {
+        //InputMaster init: creating instance of InputMaster and enabling it
         inputMaster = new InputMaster();
         inputMaster.Enable();
+        //Connecting Move function when action Movement performed
         inputMaster.Player.Movement.performed += context => Move();
     }
     void Move()
     {
+        //Getting tapPosition from Touchscreen class if on Mobile platform
         if(Application.isMobilePlatform)
         {
-            tapPosition = Camera.main.ScreenToWorldPoint(Touchscreen.current.position.ReadValue());
-            //transform.position = new Vector3(pos.x, pos.y, 0f);
-            moving = true;
+            //Touchscreen.current.position is Vector2Control for some reason, you need to cast ReadValue() to get Vector2 from it.
+            SetMovement(Camera.main.ScreenToWorldPoint(Touchscreen.current.position.ReadValue()));
         } 
+        //Getting tapPosition from Mouse class in Editor
         if(Application.isEditor)
         {
-            tapPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            //transform.position = new Vector3(pos.x, pos.y, 0f);
-            moving = true;
+            //Mouse.current.position is Vector2Control for some reason, you need to cast ReadValue() to get Vector2 from it.
+            SetMovement(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
         } 
     }
-    // Update is called once per frame
+    void SetMovement(Vector3 position)
+    {
+        tapPosition = position;
+        //Flattening tapPosition
+        tapPosition = new Vector3(tapPosition.x, tapPosition.y, 0f)
+;       //Rising flags
+        moving = true;
+        needRotation = true;
+    }
     void Update()
     {
-        Vector3 direction = new Vector3(tapPosition.x, tapPosition.y, 0f) - transform.position;
+        //Getting direction
+        Vector3 direction = tapPosition - transform.position;
+        //Rotating when needRotation flag is rised
+        if(needRotation)
+        {
+            transform.Rotate(0f, 0f, Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg);
+            needRotation = false;
+        }
+        //Moving in direction with our speed until we in deltaRadius from tapPosition
         if(moving)
-        {
             transform.position += Vector3.Normalize(direction) * speed * Time.deltaTime;
-        }
         if(direction.magnitude <= deltaRadius)
-        {
-            moving = false;
-        }
+            moving = false;  
     }
 }
