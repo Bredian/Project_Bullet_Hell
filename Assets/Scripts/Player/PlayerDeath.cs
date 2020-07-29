87595c378b48b1f4434be0ba41b80059c42e4f79
Bredian <sasha.kry@gmail.com>
2020-07-29 23:10:59 +0700

    Player Effects + Basic Projectile
    2 effects of player: moving trail, explosion.
    Standing projectile kills player with explosion effect

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem explosion;
+    [SerializeField] private SpriteRenderer sprite;
+    private bool dead = false;
+    public void Death()
+    {
+        dead = true;
+        sprite.enabled = false;
+        explosion.Play();
+    }
+    void Update()
+    {
+        if(!explosion.isPlaying && dead)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/ProjectileBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+        void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Player_Movement>().speed = 0;
+            col.gameObject.GetComponent<PlayerDeath>().Death();
+        }
+    }
+}
